0a407395022fd83932141ab94abb966044bc33b5
Johan_David <jhoanpor78@gmail.com>
2022-01-03 13:34:59 -0500

    Backend V3

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use(cors);
+app.use(cors());
 
 // Router
 app.use(require('./routers/index'));
